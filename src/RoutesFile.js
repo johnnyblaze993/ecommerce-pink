826c861f79fd078d3522f9aa1087632af0199279
johnnyblaze993 <107284554+johnnyblaze993@users.noreply.github.com>
2022-12-05 09:57:10 -0500

    yes

--- src/RoutesFile.js
+++ src/RoutesFile.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Product from "./components/Product";
-import Products from "./components/Products";
-import Home from "./components/Home";
+import Product from "./pages/Product";
+import Products from "./pages/Products";
+import Home from "./pages/Home";
 import Header from "./components/Header";
-import Cart from "./components/Cart";
-import Category from "./components/Category";
+import Cart from "./pages/Cart";
+import Category from "./pages/Category";
 import Footer from "./components/Footer";
 
 const RoutesFile = () => {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import HomeBanner from "./HomeBanner";
+import HomeBanner from "../components/HomeBanner";
 
 const Home = () => {
   const navigate = useNavigate();
